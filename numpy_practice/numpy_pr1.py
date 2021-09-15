e6f2fe3c7b8056f08b1a3b9d8ad0d6c0a89ca3b3
soldatsm <soldatsm@yandex.ru>
2021-09-15 16:51:14 +0300

    commit 1

--- numpy_practice/numpy_pr1.py
+++ numpy_practice/numpy_pr1.py
@@ -1,8 +1,24 @@
 import numpy as np
-
 # x = np.array([[2, 3, 4], [3, 4, 5]])
 
-a = np.array([0, 2, 1])
-print(a, type(a))
+# a = np.array([0, 2, 1])
+# print(a, type(a))
+
+# help(np.dot)
+# help(np.maximum)
+##
+
+#inputs = np.array([1, 2, 3, 2.5])
+#weights1 = np.array([0.2, 0.8, -0.5, 1.0])
+#weights2 = np.array([0.5, -0.91, 0.26, -0.5])
+#weights3 = np.array([-0.26, -0.27, 0.17, 0.87])
+#bias1 = 2
+#bias2 = 3
+#bias3 = 0.5
+#output = [inputs[0]*weights1[0]+inputs[1]*weights1[1]+inputs[2]*weights1[2]+inputs[3]*weights1[3]+bias1,
+         # inputs[0]*weights2[0]+inputs[1]*weights2[1]+inputs[2]*weights2[2]+inputs[3]*weights2[3]+bias2,
+          #inputs[0]*weights3[0]+inputs[1]*weights3[1]+inputs[2]*weights3[2]+inputs[3]*weights3[3]+bias3]
+#print(output)
+
 
 
